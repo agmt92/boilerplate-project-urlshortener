2790ecfe869d212b2dce8d4f1e9d08c3a7eb94b1
agmt92 <155798390+agmt92@users.noreply.github.com>
2024-08-17 19:28:27 +0200

    feature: configured mongoose, connected, set-up the schema

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose'); // Import mongoose
+
 const app = express();
 
 // Basic Configuration
@@ -19,6 +21,28 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to MongoDB');
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+// Define URL Schema
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+module.exports = app;
